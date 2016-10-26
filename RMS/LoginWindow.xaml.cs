d65a440f237490c8dadfb606d2caecefc0d8431e
Kurt Gao <kurtgao@outlook.com>
2016-10-26 18:51:45 +0800

    login window deployed

---
+++ RMS/LoginWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data;
+
+namespace RMS
+{
+    public partial class Login : Window
+    {
+        static string rms = "Database='RMS';DataSource = 'localhost'; User Id = 'root'; Password = 'root'; charset = 'utf8'";
+        MySqlConnection con = new MySqlConnection(rms);
+        public Login()
+        {
+            InitializeComponent();
+            try
+            {
+                con.Open();
+            } catch (MySqlException e)
+            {
+                    MessageBox.Show("Fail to connect SQL server!\n\nError Code: " + e.Number);
+                    this.Close();
+            }
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            IntPtr p = System.Runtime.InteropServices.Marshal.SecureStringToBSTR(this.tbPassword.SecurePassword);
+            string password = System.Runtime.InteropServices.Marshal.PtrToStringBSTR(p);
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = ("select role from staff where account='"+tbAccount.Text+"' and password='"+password+"'");
+            if (cmd.ExecuteScalar()!=null)
+            {
+                MainWindow mWindow = new MainWindow();
+                mWindow.getRole((string)cmd.ExecuteScalar());
+                mWindow.getAccount(tbAccount.Text);
+                this.Close();
+                mWindow.ShowDialog();     
+            }
+            else
+            {
+                MessageBox.Show("Incorrect account or password.");
+            }
+        }
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ RMS/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace RMS
+{
+    public partial class MainWindow : Window
+    {
+        static string account;
+        static string role;
+        public void getAccount(string e)
+        {
+            account = e;
+        }
+        public void getRole(string e)
+        {
+            role = e;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(account+" "+role);
+        }
+    }
+}
